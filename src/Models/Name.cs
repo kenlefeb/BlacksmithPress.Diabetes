da8fae6b6ad79135c499b654fdec292ee3660ea9
Kenneth LeFebvre <kenneth@lefebvre.us>
2019-08-02 23:02:40 -0400

    Added tests for Name.ToString() and implemented to pass.

--- src/Models/Name.cs
+++ src/Models/Name.cs
@@ -1,9 +1,63 @@
-﻿namespace Diabetes.Models
+﻿using System.Text;
+
+namespace Diabetes.Models
 {
     public class Name
     {
-        public string First { get; set; } = string.Empty;
-        public string Middle { get; set; } = string.Empty;
-        public string Last { get; set; } = string.Empty;
+        private string _first = string.Empty;
+        private string _middle = string.Empty;
+        private string _last = string.Empty;
+
+        public string First
+        {
+            get => _first;
+            set => _first = Normalize(value);
+        }
+
+        public string Middle
+        {
+            get => _middle;
+            set => _middle = Normalize(value);
+        }
+
+        public string Last
+        {
+            get => _last;
+            set => _last = Normalize(value);
+        }
+
+        private string Normalize(string name)
+        {
+            name = name?.Trim() ?? string.Empty;
+            if (name.Length == 1)
+                name += ".";
+            return name;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+            if (!string.IsNullOrEmpty(First))
+                builder.Append(First);
+
+            AppendName(builder, Middle);
+            AppendName(builder, Last);
+
+            return builder.ToString();
+        }
+
+        private void AppendName(StringBuilder builder, string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                if (builder.Length > 0)
+                    builder.Append(" ");
+
+                builder.Append(name);
+
+                if (name.Length == 1)
+                    builder.Append(".");
+            }
+        }
     }
 }
--- src/Tests.Models/Users.cs
+++ src/Tests.Models/Users.cs
@@ -20,5 +20,32 @@ namespace Tests.Models
             actual.Name.Middle.Should().NotBeNull("the names should be initialized to an empty string");
             actual.Name.Last.Should().NotBeNull("the names should be initialized to an empty string");
         }
+
+        [Theory]
+        [InlineData("John", "Q", "Public", "John Q. Public")]
+        [InlineData("John ", "Q", "Public", "John Q. Public")]
+        [InlineData("John", "Q ", "Public", "John Q. Public")]
+        [InlineData("John", "Q", "Public ", "John Q. Public")]
+        public void CastNameToString_ConcatenatesFirstMiddleLast(string first, string middle, string last, string expected)
+        {
+            // arrange isolation
+
+            // arrange test
+            var subject = new Name
+            {
+                First = first,
+                Middle = middle,
+                Last = last
+            };
+
+            // act
+            var actual = $"{subject}";
+
+            // assert
+            actual.Should().Be(expected);
+
+            // clean-up
+        }
+
     }
 }
